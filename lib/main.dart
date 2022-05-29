96f08a84e53ad0ad6c09fdd18ed61e8d8ab16d0a
Halit Mesut <halitmesut@gmail.com>
2022-05-30 01:01:43 +0300

    created expanded type functions for clearing repetative codes

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:just_audio/just_audio.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   void playSound(String animalName) {
     final player = AudioPlayer();
     player.setAsset('assets/animalSound/$animalName.wav');
@@ -21,9 +23,22 @@ class MyApp extends StatelessWidget {
         },
         child: ImageIcon(
           AssetImage("assets/$animalName.png"),
-          color: Color(0xFF3A5A98),
+          color: const Color(0xFF3A5A98),
           size: 70,
         ),
+      ),
+    );
+  }
+
+  Expanded rowButtons(
+      {required String leftAnimal, required String rightAnimal}) {
+    return Expanded(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          buildKey(animalName: leftAnimal),
+          buildKey(animalName: rightAnimal),
+        ],
       ),
     );
   }
@@ -35,58 +50,18 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
-          backgroundColor: Color.fromRGBO(70, 91, 148, 1),
+          backgroundColor: const Color.fromRGBO(70, 91, 148, 1),
           title: const Text("Animal Sound"),
           centerTitle: true,
         ),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  buildKey(animalName: "bird"),
-                  buildKey(animalName: "duck"),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  buildKey(animalName: "cat"),
-                  buildKey(animalName: "dog"),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  buildKey(animalName: "chicken"),
-                  buildKey(animalName: "rooster"),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  buildKey(animalName: "horse"),
-                  buildKey(animalName: "elephant"),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  buildKey(animalName: "tiger"),
-                  buildKey(animalName: "lion"),
-                ],
-              ),
-            ),
+            rowButtons(leftAnimal: "bird", rightAnimal: "duck"),
+            rowButtons(leftAnimal: "cat", rightAnimal: "dog"),
+            rowButtons(leftAnimal: "chicken", rightAnimal: "rooster"),
+            rowButtons(leftAnimal: "horse", rightAnimal: "elephant"),
+            rowButtons(leftAnimal: "tiger", rightAnimal: "lion"),
           ],
         ),
       ),
